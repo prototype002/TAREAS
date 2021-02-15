a56800606812c08c2d6dc42668b04116ff2aa53f
prototype002 <61293804+prototype002@users.noreply.github.com>
2021-02-14 23:42:36 -0400

    Add files via upload

---
+++ tarea1/tarea1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace rectangle
+{
+	class programa
+	{
+		static void Main(string[] args)
+		{
+			rectangle rentangle1 = new rectangle();
+			rectangle.basis = 3;
+			rectangle.height = 7;
+
+			rectangle rentangle2 = new rectangle();
+			rectangle.basis = (int)1.5;
+			rectangle.height = 4;
+
+			Console.WriteLine(rectangle1.Compare(rectangle2));
+
+
+			Console.ReadKey();
+			}
+		}
+	}
---
+++ tarea1/tarea1/resctangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle
+{
+	public class rectangle
+	{
+		public double basis;
+		public double height;
+
+		public double Area()
+		{
+
+			Double Area = this.basis * this.height;
+			return Area;
+		}
+
+		public double perimeter()
+		{
+			double perim = this.basis + this.basis + this.height;
+			return perim;
+		}
+
+		public void Grow(double quantity)
+		{
+
+			this.basis = this.basis + quantity;
+			this.height = this.height + quantity;
+		}
+
+		public double Compare(rectangle rect)
+		{
+
+			return this.Area() - rect.Area();
+		}
+
+
+
+		
+		}
+	}
+}
+
